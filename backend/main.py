685ffafb2f1b0dedb66e76edc4ba9ca728b2f541
GreenHexagon <irrelevantpersonindividual@gmail.com>
2025-11-07 15:26:17 -0500

    Extend backend

--- backend/main.py
+++ backend/main.py
@@ -17,16 +17,6 @@ backend.add_middleware(
     allow_headers = ["*"]
 )
 
-class UserModel(BaseModel):
-    id: int
-    username: str
-    password: str
-
-
-class MessageBase(BaseModel):
-    message: str
-    user: UserModel | str 
-
 
 
 @backend.get("/")
